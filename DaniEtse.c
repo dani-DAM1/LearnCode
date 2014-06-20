ba445ae469021a390d9a6d46bce94cb65418eb81
dani-DAM1 <picamoixonenc@gmail.com>
2014-06-20 10:59:57 +0200

    d

--- DaniEtse.c
+++ DaniEtse.c
@@ -1,25 +1,37 @@
 #include<stdio.h>
+#include<time.h>
 #define ASCII 65
 int main(void){
 
-    int quantitatParelles,figura=ASCII,X,Y,x,y,i,j;
+    int quantitatParelles,figura=ASCII,X=0,Y=0,x,y,i,j;
     srand(time(NULL));
+
     printf ("intro parelles");
     scanf("%d",&quantitatParelles);
     int taulell[2][quantitatParelles];
 
     for(i=0;i<2;i++){
+        printf("\n");
         for(j=0;j<quantitatParelles;j++){
-            X=rand()%quantitatParelles;
-            Y=rand()%quantitatParelles;
-            taulell[X][Y]=figura;
-            do{
-            x=rand()%quantitatParelles;
-            y=rand()%quantitatParelles;
-            }while(X==x||Y==y);
-            taulell[x][y]=figura;
-            figura++;
+            printf("| %d ",taulell[i][j]);
         }
+    }
+
+    for(i=0;i<2;i++){
+            for(j=0;j<=quantitatParelles;j++){
+                do{
+                    X=rand()%quantitatParelles-1;
+                    Y=rand()%quantitatParelles-1;
+                }while(taulell[X][Y]!=0);
+                taulell[X][Y]=figura;
+                do{
+                    x=rand()%quantitatParelles;
+                    y=rand()%quantitatParelles;
+                }while((X==x||Y==y)&&(taulell[X][Y]!=0));
+                taulell[x][y]=figura;
+                figura++;
+            }
+
     }
     for(i=0;i<2;i++){
         printf("\n");
