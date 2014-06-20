b01614c87395b81eb2328301603862152c24b793
dlinan <daniel.linan.cmrm11@gmail.com>
2014-06-20 10:27:22 +0200

    memory_versio1_04.5

--- memory_versio1.c
+++ memory_versio1.c
@@ -1,11 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void omplirmatriu(){
 
-void main(){
+}
+
+
+int main(){
 int parelles;
+char teclainici;
+srand(time(NULL));
 printf("Digues el nombre de parelles que vols:\n");
 scanf("%d",&parelles);
 getchar();
 int taulell[2][parelles];
+omplirmatriu();
+printf("Prem qualsevol tecla per iniciar el joc.\n");
+scanf("%c");
+getchar();
+return 0;
 }
