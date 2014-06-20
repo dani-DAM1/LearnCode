f3701aba81c6e875f5dcd417d4bd5867deaf6d4b
dlinan <daniel.linan.cmrm11@gmail.com>
2014-06-20 11:03:16 +0200

    memory_versio1_06

--- memory_versio1.c
+++ memory_versio1.c
@@ -1,22 +1,46 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#define ASCII 65
 
-void omplirmatriu(){
+void omplirTaulell();
+void mostrarTaulell();
+void amagarCartes();
 
+int main (){
+int quantitatParelles,teclaInici,figura=ASCII,X,Y,x,y,i,j;
+srand(time(NULL));
+
+printf ("Benvingut al joc 'Memory'.\n\nQuina quantitat de parelles vols?: ");
+scanf ("%d",&quantitatParelles);
+int taulell[2][quantitatParelles];
+for(i=0;i<2;i++){
+    for(j=0;j<quantitatParelles;j++){
+        X=rand()%quantitatParelles;
+        Y=rand()%quantitatParelles;
+        taulell[x][y]=figura;
+        do{
+            x=rand()%quantitatParelles;
+            y=rand()%quantitatParelles;
+            }while(X==x||Y==y);
+            taulell[x][y]=figura;
+        }
+        figura++;
+    }
+
+//mostrarTaulell ();
+
+printf ("\nPrem qualsevol tecla per amagar les cartes i comen�ar el joc\n");
+scanf ("%d",&teclaInici);
+
+//amagarCartes();
+
+return 0;
 }
 
 
-int main(){
-int parelles;
-char teclainici;
-srand(time(NULL));
-printf("Digues el nombre de parelles que vols:\n");
-scanf("%d",&parelles);
-getchar();
-int taulell[2][parelles];
-omplirmatriu();
-printf("Prem qualsevol tecla per iniciar el joc.\n");
-
-getchar();
-return 0;
+void mostrarTaulell(){
+}
+void amagarCartes (){
 }
