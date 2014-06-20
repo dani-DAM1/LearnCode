afec6a36c8fd0ce0196fdb363165160fcb2c46d0
anakonda89 <anakonda_apb@hotmail.com>
2014-06-20 10:31:19 +0200

    memory v2.0

---
+++ provesasdf.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define ASCII 65
+
+void omplirTaulell();
+void mostrarTaulell();
+void amagarCartes();
+
+int main (){
+int quantitatParelles,teclaInici,figura=ASCII,X,Y,x,y,i,j;
+srand(time(NULL));
+
+printf ("Benvingut al joc 'Memory'.\n\nQuina quantitat de parelles vols?: ");
+scanf ("%d",&quantitatParelles);
+int taulell[2][quantitatParelles];
+for(i=0;i<2;i++){
+    for(j=0;j<quantitatParelles;j++){
+        X=rand()%quantitatParelles;
+        Y=rand()%quantitatParelles;
+        taulell[x][y]=figura;
+        do{
+            x=rand()%quantitatParelles;
+            y=rand()%quantitatParelles;
+            }while(X==x||Y==y);
+            taulell[x][y]=figura;
+        }
+        figura++;
+    }
+
+//mostrarTaulell ();
+
+printf ("\nPrem qualsevol tecla per amagar les cartes i començar el joc\n");
+scanf ("%d",&teclaInici);
+
+//amagarCartes();
+
+return 0;
+}
+
+
+void mostrarTaulell(){
+}
+void amagarCartes (){
+}
