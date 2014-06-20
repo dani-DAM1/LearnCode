6737d6a10ea199915c0579804daf232f0ea4ebf5
dani-DAM1 <picamoixonenc@gmail.com>
2014-06-20 10:20:25 +0200

    daniG

--- DaniEtse.c
+++ DaniEtse.c
@@ -1,1 +1,26 @@
-Etsecode
+#include<stdio.h>
+#define ASCII 65
+int main(void){
+
+    int quantitatParelles,figura=ASCII,X,Y,x,y;
+    srand(time(NULL));
+    printf ("intro parelles");
+    scanf("%d",&quantitatParelles);
+    int taulell[2][quantitatParelles];
+
+    for(i=0;i<2;i++){
+        for(j=0;j<quantitatParelles;j++){
+            X=rand()%quantitatParelles;
+            Y=rand()%quantitatParelles;
+            taulell[x][y]=figura;
+            figura++;
+            do{
+            x=rand()%quantitatParelles;
+            y=rand()%quantitatParelles;
+            }while(X==x&&Y==y);
+            taulell[x][y]=figura;
+            figura++;
+        }
+    }
+
+}
