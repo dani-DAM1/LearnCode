036d748988033bf007390d67c6fb26031ae8f81c
dlinan <daniel.linan.cmrm11@gmail.com>
2014-06-20 10:06:23 +0200

    memory_versio1_04

--- memory_versio1.c
+++ memory_versio1.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
-int parelles;
-int taulell[2][parelles];
-void main(){
 
+
+void main(){
+int parelles;
 printf("Digues el nombre de parelles que vols:\n");
 scanf("%d",&parelles);
 getchar();
+int taulell[2][parelles];
 }
